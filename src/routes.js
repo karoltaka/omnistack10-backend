aff542ef58bafcbb087ff43e9f5494f3b21fa681
karoltaka <takahagassi.dev@gmail.com>
2020-05-06 17:36:52 -0300

    dev routes

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,33 @@
 const { Router } = require('express');
 const axios = require('axios');
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
 routes.post('/devs', async (req, res) => {
-  const { github_username } = req.body;
+  const { github_username, techs, latitude, longitude } = req.body;
 
   const apiRes = await axios.get(`https://api.github.com/users/${github_username}`);
 
-  const { name, avatar_url, bio } = apiRes.data;
+  const { name = login, avatar_url, bio } = apiRes.data;
 
-  return res.json({ message: 'Hello Omnistack'});
+  const techsArray = techs.split(',').map(tech => tech.trim());
+
+  const location = {
+    type: 'Point',
+    coordinates: [longitude, latitude],
+  };
+
+  const dev = await Dev.create({
+    github_username,
+    name,
+    avatar_url,
+    bio,
+    techs: techsArray,
+    location,
+  })
+
+  return res.json(dev);
 });
 
 module.exports = routes;
