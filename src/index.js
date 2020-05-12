aae55893d4409da63cfe517835d3835f7a444f7b
karoltaka <takahagassi.dev@gmail.com>
2020-05-12 19:45:15 -0300

    added cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -10,6 +11,7 @@ mongoose.connect('mongodb://admin:admin123@ds045252.mlab.com:45252/devradar',{
   useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
