d07d30ef131377be98a932a84034298e76ba3c32
karoltaka <takahagassi.dev@gmail.com>
2020-05-19 11:35:51 -0300

    added websocket

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
+
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket')
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 // Banco de dados
 mongoose.connect('mongodb://admin:admin123@ds045252.mlab.com:45252/devradar',{
@@ -15,4 +21,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
